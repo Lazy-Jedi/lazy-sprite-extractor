4e36c1d18995cfbb82207831c16ef9ce7e913036
Ubaidullah Effendi-Emjedi <mr.ueemjedi@gmail.com>
2019-05-20 06:14:33 +0200

    Update
    Complete revamp of the Core Editor Window and its Features.

---
+++ Assets/SpriteExtractor/Editor/SpriteExtractorWindow.cs
@@ -0,0 +1,256 @@
+﻿/*
+ *
+ * Created By: Uee
+ * Modified By: 
+ *
+ * Last Modified: 19 May 2019
+ *
+ */
+
+using System.IO;
+using SpriteExtractor.Extensions;
+using SpriteExtractor.Utilities;
+using UnityEditor;
+using UnityEngine;
+
+namespace SpriteExtractor.Editor
+{
+	public class SpriteExtractorWindow : EditorWindow
+	{
+		#region EDITOR WINDOW
+
+		private static SpriteExtractorWindow _window;
+
+		[MenuItem("Window/Sprite Extractor")]
+		public static void CreateWindow()
+		{
+			_window = GetWindow<SpriteExtractorWindow>("Sprite Extractor Tool");
+			_window.Show();
+		}
+
+		#endregion
+
+
+		#region VARIABLES
+
+		/// <summary>
+		/// Field Colors
+		/// </summary>
+		private readonly Color _colorFields = new Color(0.54f, 0.44f, 1f);
+
+		/// <summary>
+		/// Window Color.
+		/// </summary>
+		private readonly Color _colorWindow = new Color(0.35f, 0.64f, 1f);
+
+		/// <summary>
+		/// Sprite Extractor
+		/// </summary>
+		private readonly Extractor _extractor = new Extractor();
+
+		#endregion
+
+
+		#region PROPERTIES
+
+		/// <summary>
+		/// Selected Sprite Sheet with Sub Sprites to Extract.
+		/// </summary>
+		private Texture2D SpriteSheet
+		{
+			get => _extractor?.SpriteSheet;
+			set => _extractor.SpriteSheet = value;
+		}
+
+		/// <summary>
+		/// Sprite Sheet Location Path.
+		/// </summary>
+		private string SpriteSheetPath
+		{
+			get => _extractor.SpriteSheetPath;
+			set
+			{
+				_extractor.SpriteSheetPath = value;
+				Extension                  = Path.GetExtension(SpriteSheetPath);
+			}
+		}
+
+		/// <summary>
+		/// Save Location of the Sub Sprites.
+		/// </summary>
+		private string SavePath
+		{
+			get => _extractor.SavePath;
+			set => _extractor.SavePath = value;
+		}
+
+		/// <summary>
+		/// Sprite Sheet Texture Extension.
+		/// </summary>
+		private string Extension
+		{
+			get => _extractor.Extension;
+			set => _extractor.Extension = value;
+		}
+
+		/// <summary>
+		/// Editor Window Width
+		/// </summary>
+		private float Width => position.width;
+
+
+		/// <summary>
+		/// Editor Window Height
+		/// </summary>
+		private float Height => position.height;
+
+		#endregion
+
+
+		#region GUI METHOD
+
+		private void OnGUI()
+		{
+			GUI.backgroundColor = _colorWindow;
+			EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+
+			// ToDo : Settings
+			Settings();
+			// ToDo : Display Sprite Sheet
+			DisplayTexture2D();
+			// ToDo : Extract Sprites Button
+			ExtractButton();
+
+			EditorGUILayout.EndVertical();
+		}
+
+		#endregion
+
+
+		#region GUI METHODS
+
+		/// <summary>
+		/// All the Required Setting Fields, Labels and Buttons.
+		/// </summary>
+		private void Settings()
+		{
+			GUI.backgroundColor = _colorFields;
+			SpriteSheetField();
+			LabelFields();
+			PathLocationButtons();
+		}
+
+
+		/// <summary>
+		/// Sprite Sheet Field.
+		/// </summary>
+		private void SpriteSheetField()
+		{
+			EditorGUI.BeginChangeCheck();
+
+			SpriteSheet = EditorGUILayout.ObjectField(
+				new GUIContent("Sprite Sheet", SpriteSheet, "Sprite Sheet"), SpriteSheet, typeof(Texture2D)
+				, false) as Texture2D;
+
+			if (EditorGUI.EndChangeCheck())
+			{
+				if (SpriteSheet != null && !SpriteSheet.isReadable)
+				{
+					SpriteSheet.MakeReadable(true);
+					ResetLabels();
+				}
+				else if (SpriteSheet != null)
+				{
+					ResetLabels();
+				}
+				else
+				{
+					SpriteSheetPath = SavePath = "No Sprite Sheet Selected!";
+				}
+			}
+		}
+
+		/// <summary>
+		/// Sprite Sheet and Save Path Location Labels.
+		/// </summary>
+		private void LabelFields()
+		{
+			EditorGUILayout.LabelField(new GUIContent("Sprite Sheet Location: ", SpriteSheet)
+				, new GUIContent(SpriteSheetPath), EditorStyles.textField);
+
+
+			EditorGUILayout.LabelField(new GUIContent("Save Location", "Save location of the Extracted Sprites.")
+				, new GUIContent(SavePath), EditorStyles.textField);
+		}
+
+
+		/// <summary>
+		/// Path Location Buttons.
+		/// </summary>
+		private void PathLocationButtons()
+		{
+			EditorGUILayout.BeginHorizontal();
+
+			if (GUILayout.Button(new GUIContent("New Save Location", "Choose a Different Save Location.")
+				, EditorStyles.miniButtonLeft))
+			{
+				SavePath = EditorUtility.SaveFolderPanel("Save Location", "Assets/", "");
+			}
+
+			if (GUILayout.Button(new GUIContent("Reset Save Location"
+					, "Reset the Save Location of the Sprites to the Default Sprite Sheet Path Location.")
+				, EditorStyles.miniButtonRight))
+			{
+				SavePath = SpriteSheet != null
+					? SpriteSheetPath.Replace(SpriteSheet.name + Extension, "")
+					: "No Sprite Sheet Selected!";
+			}
+
+			EditorGUILayout.EndHorizontal();
+		}
+
+
+		/// <summary>
+		/// Display the Sprite Sheet.
+		/// </summary>
+		private void DisplayTexture2D()
+		{
+			// ToDo : Display Sprite Sheet Correctly.
+			if (SpriteSheet != null)
+			{
+				GUI.DrawTexture(new Rect(32, 196, Width - 64, Height - 264), SpriteSheet, ScaleMode.ScaleToFit);
+			}
+		}
+
+		/// <summary>
+		///  Extract Sub Sprites Button.
+		/// </summary>
+		private void ExtractButton()
+		{
+			if (GUILayout.Button(
+				new GUIContent("Extract Sprites?", "Extract all the Sub Sprites from the Sprite Sheet.")
+				, EditorStyles.miniButton))
+			{
+				if (SpriteSheet != null)
+					_extractor.ExtractSprites();
+			}
+		}
+
+		#endregion
+
+
+		#region HELPER METHODS
+
+		/// <summary>
+		/// Reset Labels.
+		/// </summary>
+		private void ResetLabels()
+		{
+			SpriteSheetPath =
+				SpriteSheet != null ? AssetDatabase.GetAssetPath(SpriteSheet) : "No Sprite Sheet Selected";
+			SavePath = SpriteSheetPath.Replace(SpriteSheet.name + Extension, "");
+		}
+
+		#endregion
+	}
+}
--- Assets/SpriteExtractor/Extensions/Texture2DExtensions.cs
+++ Assets/SpriteExtractor/Extensions/Texture2DExtensions.cs
@@ -3,21 +3,12 @@
  * Created By: Uee
  * Modified By: 
  *
- * Last Modified: 12 February 2019
+ * Last Modified: 16 April 2019
  *
  *
  * This software is released under the terms of the
  * GNU license. See https://www.gnu.org/licenses/#GPL
  * for more information.
- *
- *
- * Important
- * Whatever Artwork is used in conjunction with this tool and is extracted using this tool has no 
- * affiliation with this tool or its creator.
- * 
- * The Artwork(s) belong to their respective creators (the individuals that made them) 
- * and the user of this tool should adhere to the respective licenses stipulated by the 
- * owners/creators of the Artwork.
  *
  */
 
@@ -26,95 +17,93 @@ using UnityEngine;
 
 namespace SpriteExtractor.Extensions
 {
-    public static class Texture2DExtensions
-    {
-        #region VARIABLES
+	public static class Texture2DExtension
+	{
+		#region VARIABLES
 
-        private static Color[] _pixelData;
-        private static Texture2D _texture2D;
-        private static TextureImporter _textureImporter;
+		/// <summary>
+		/// Cached Pixel Color Data of Sprite.
+		/// </summary>
+		private static Color[] _pixelData;
 
-        #endregion
+		/// <summary>
+		/// Cached Sprite Texture 2D.
+		/// </summary>
+		private static Texture2D _texture2D;
+
+		/// <summary>
+		/// Cached Texture Importer used to Modify Texture Import Settings.
+		/// </summary>
+		private static TextureImporter _textureImporter;
+
+		#endregion
 
 
-        #region EXTENSION METHODS
+		#region EXTENSION METHODS
 
-        public static Texture2D CropTexture2D(this Texture2D texture2D, int x, int y, int width, int height)
-        {
-            /* Checks if the crop data (x, y, width & height) are within the boundaries of the Original Texture. */
-            if (x < 0)
-            {
-                width += x;
-                x     =  0;
-            }
+		/// <summary>
+		/// Crop Textures from Sprite Bounds.
+		/// </summary>
+		/// <param name="texture2D"></param>
+		/// <param name="x"></param>
+		/// <param name="y"></param>
+		/// <param name="width"></param>
+		/// <param name="height"></param>
+		/// <returns></returns>
+		public static Texture2D CropTexture2D(this Texture2D texture2D, int x, int y, int width, int height)
+		{
+			/* Checks if the crop data (x, y, width & height) are within the boundaries of the Original Texture. */
+			if (x < 0)
+			{
+				width += x;
+				x     =  0;
+			}
 
-            if (y < 0)
-            {
-                height += y;
-                y      =  0;
-            }
+			if (y < 0)
+			{
+				height += y;
+				y      =  0;
+			}
 
-            if (x + width > texture2D.width)
-            {
-                width = texture2D.width - x;
-            }
+			if (x + width > texture2D.width) width = texture2D.width - x;
 
-            if (y + height > texture2D.height)
-            {
-                height = texture2D.height - y;
-            }
+			if (y + height > texture2D.height) height = texture2D.height - y;
 
-            if (width <= 0 || height <= 0)
-            {
-                return null;
-            }
+			if (width <= 0 || height <= 0) return null;
 
-            /* Get the Pixel Data of the Specified Region within the Texture. */
-            _pixelData = texture2D.GetPixels(x, y, width, height);
+			/* Get the Pixel Data of the Specified Region within the Texture. */
+			_pixelData = texture2D.GetPixels(x, y, width, height);
 
-            /* Make a new Texture2D */
-            _texture2D = new Texture2D(width, height, TextureFormat.RGBA32, false);
+			/* Make a new Texture2D */
+			_texture2D = new Texture2D(width, height, TextureFormat.RGBA32, false);
 
-            /* Overwrite the old Pixel Data with the New Extracted Pixel Data. */
-            _texture2D.SetPixels(_pixelData);
+			/* Overwrite the old Pixel Data with the New Extracted Pixel Data. */
+			_texture2D.SetPixels(_pixelData);
 
-            /* Save the Changes to the new Texture. */
-            _texture2D.Apply();
+			/* Save the Changes to the new Texture. */
+			_texture2D.Apply();
 
-            /* Return the Cropped out Texture. */
-            return _texture2D;
-        }
+			/* Return the Cropped out Texture. */
+			return _texture2D;
+		}
 
-        /// <summary>
-        /// Check the Readable Import Property of the Texture
-        /// </summary>
-        /// <param name="source"></param>
-        /// <returns></returns>
-        public static bool IsReadable(this Texture2D source)
-        {
-            string texturePath = AssetDatabase.GetAssetPath(source);
-            _textureImporter = AssetImporter.GetAtPath(texturePath) as TextureImporter;
+		/// <summary>
+		///     Change the Readable Import Property of the Texture.
+		/// </summary>
+		/// <param name="source"></param>
+		/// <param name="readable"></param>
+		public static void MakeReadable(this Texture2D source, bool readable)
+		{
+			string texturePath = AssetDatabase.GetAssetPath(source);
+			_textureImporter = AssetImporter.GetAtPath(texturePath) as TextureImporter;
 
-            return _textureImporter != null && _textureImporter.isReadable;
-        }
+			if (_textureImporter == null) return;
 
-        /// <summary>
-        /// Change the Readable Import Property of the Texture
-        /// </summary>
-        /// <param name="source"></param>
-        /// <param name="readable"></param>
-        public static void MakeReadable(this Texture2D source, bool readable)
-        {
-            string texturePath = AssetDatabase.GetAssetPath(source);
-            _textureImporter = AssetImporter.GetAtPath(texturePath) as TextureImporter;
+			_textureImporter.isReadable = readable;
+			AssetDatabase.ImportAsset(texturePath);
+			AssetDatabase.Refresh();
+		}
 
-            if (_textureImporter == null) return;
-
-            _textureImporter.isReadable = readable;
-            AssetDatabase.ImportAsset(texturePath);
-            AssetDatabase.Refresh();
-        }
-
-        #endregion
-    }
+		#endregion
+	}
 }
